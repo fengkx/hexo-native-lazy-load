2afadfa309da1c568a79eac8316bb8fcbce71c56
fengkx <liangkx8237@gmail.com>
2019-09-15 19:56:47 +0800

    style: use preitter

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const {addAttr, log, addScript} = require('./lib');
-hexo.debug=true;
+const { addAttr, log, addScript } = require("./lib");
+hexo.debug = true;
 
 if (!hexo.config.lazy_load || !hexo.config.lazy_load.enable) {
-    return;
+  return;
 }
-if (hexo.config.lazy_load.all) { // default only posts
-    log.info('Add lazy load attribute to all image');
-    hexo.extend.filter.register('after_render:html', addAttr.all, 15)
+if (hexo.config.lazy_load.all) {
+  // default only posts
+  log.info("Add lazy load attribute to all image");
+  hexo.extend.filter.register("after_render:html", addAttr.all, 15);
 } else {
-    hexo.extend.filter.register('after_post_render', addAttr.posts, 15);
+  hexo.extend.filter.register("after_post_render", addAttr.posts, 15);
 }
-if (hexo.config.lazy_load.fallback !== false) { // default enable
-    log.info('Add fallback lazy load using lazysizes');
-    hexo.extend.filter.register('after_render:html', addScript, 25)
+if (hexo.config.lazy_load.fallback !== false) {
+  // default enable
+  log.info("Add fallback lazy load using lazysizes");
+  hexo.extend.filter.register("after_render:html", addScript, 25);
 }
