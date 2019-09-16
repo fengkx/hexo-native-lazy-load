d512bbce529dc35f3dd82739c98da10788805d3e
fengkx <liangkx8237@gmail.com>
2019-09-16 17:17:19 +0800

    fix: remove reqire hexo-log

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const { addAttr, log, addScript } = require("./lib");
-hexo.debug = true;
-
+const { addAttr, addScript } = require("./lib");
+const log = hexo.log;
 if (!hexo.config.lazy_load || !hexo.config.lazy_load.enable) {
   return;
 }
@@ -9,6 +8,7 @@ if (hexo.config.lazy_load.all) {
   log.info("Add lazy load attribute to all image");
   hexo.extend.filter.register("after_render:html", addAttr.all, 15);
 } else {
+  log.info("Add lazy load attribute to all post");
   hexo.extend.filter.register("after_post_render", addAttr.posts, 15);
 }
 if (hexo.config.lazy_load.fallback !== false) {
